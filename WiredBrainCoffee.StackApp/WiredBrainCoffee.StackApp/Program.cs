0cb0896dbdb1bab1f91cfa515d180b6ce689db25
vny99 <vnyk1873@gmail.com>
2023-04-04 12:00:46 +0530

    c# stack implementation using generics

---
+++ WiredBrainCoffee.StackApp/WiredBrainCoffee.StackApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace WiredBrainCoffee.StackApp
+{
+    class program
+    {
+        static void Main(String[] args)
+        {
+            StackForDouble();
+            StackForString();
+
+        }
+
+        private static void StackForString()
+        {
+            var stack = new SimpleStack<String>();
+            stack.Push("a");
+            stack.Push("b");
+            stack.Push("c");
+            var sum="";
+            while (stack.Count() > 0)
+            {
+                String r = stack.Pop();
+                Console.WriteLine($"item - {r}");
+                sum += r;
+            }
+
+        }
+
+        private static void StackForDouble()
+        {
+            var stack = new SimpleStack<double>();
+            stack.Push(1.2);
+            stack.Push(2.8);
+            stack.Push(3.0);
+            var sum = 0.0;
+            while (stack.Count() > 0)
+            {
+                double r = stack.Pop();
+                Console.WriteLine($"item - {r}");
+                sum += r;
+            }
+        }
+    }
+}
---
+++ WiredBrainCoffee.StackApp/WiredBrainCoffee.StackApp/SimpleStack.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace WiredBrainCoffee.StackApp
+{
+    public class SimpleStack<T>
+    {
+        private T[] _items;
+        private int _currentIndex;
+        public SimpleStack()
+        {
+            _items = new T[10];
+            _currentIndex = -1;
+        }
+
+        public void Push(T item)
+        {
+            _items[++_currentIndex] = item;
+        }
+        public T Pop()
+        {
+            return _items[_currentIndex--];
+        }
+        public int Count()
+        {
+            return _currentIndex + 1;
+        }
+    }
+}
